83317abc5ff9715528893344595e65d73e04a325
Varun <vjindz@gmail.com>
2018-04-28 19:39:17 -0400

    test7

--- scraper.php
+++ scraper.php
@@ -3,13 +3,14 @@
 	$city = $_GET['city'];
 
 	$city = str_replace(" ", "-", $city);
-	ob_start();
+	// ob_start();
 	if (file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest")) {
 		$content = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-		preg_match('/</h2>(1&ndash;3 days)</span><p class="b-forecast__table-description-content"><span class="phrase">(.*?)</s',
+		preg_match('/</span><p class="b-forecast__table-description-content"><span class="phrase">(.*?)</s',
 			$content,
 			$matches);
 		echo $matches[1];
+		/*send as argument ie python python.py message */
 	} else {
 		ob_end_clean();
 		echo "";
