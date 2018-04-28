b55942ba9760d6f566b6280b786c03909ccc09c9
Varun <vjindz@gmail.com>
2018-04-28 17:57:13 -0400

    test 2

--- scraper.php
+++ scraper.php
@@ -6,10 +6,10 @@
 	ob_start();
 	if (file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest")) {
 		$content = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-		preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</i', 
-			$content, 
+		preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</i',
+			$content,
 			$matches);
-		echo $matches[1];
+		echo "got it";
 	} else {
 		ob_end_clean();
 		echo "";
