e9a16abccf45fc1400e6f7d8bd7b884100c5122b
Varun <vjindz@gmail.com>
2018-04-29 04:32:24 -0400

    test with sys

--- parseText.py
+++ parseText.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+# from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import sys
 # import urllib2
-analyzer = SentimentIntensityAnalyzer()
+# analyzer = SentimentIntensityAnalyzer()
 list = []
 # for x in range(1, len(sys.argv)):
     # list.append(analyzer.polarity_scores(sys.argv[x])["compound"])
-print(analyzer.polarity_scores(sys.argv[1])["compound"])
+# print(analyzer.polarity_scores(sys.argv[1])["compound"])
 # req = urllib2.Request(url='https://connotationdictionary.herokuapp.com/scraper.php?nums=%s' % list)
 # f = urllib2.urlopen(req)
 # print f.read()
+
+print(sys.argv[1])
