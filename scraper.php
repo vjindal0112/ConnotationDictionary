3cdf6b560b2fe4ec182d21f14c4668ace08341e0
Varun <vjindz@gmail.com>
2018-04-29 03:38:49 -0400

    test for php working

--- scraper.php
+++ scraper.php
@@ -9,8 +9,8 @@
 		preg_match_all('/<span class="def-number">[0-9].<\/span>\s*<div class="def-content">(.*?)</s',
 			$content,
 			$matches);
-		$result = shell_exec('python parseText.py ' . $matches[1][0]);
-		echo $result;
+		// $result = shell_exec('python parseText.py ' . $matches[1][0]);
+		echo $matches[1][0];
 
 		// $nums = $_GET["nums"];
 		// echo $nums[0]
