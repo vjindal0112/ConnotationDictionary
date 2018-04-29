5d18aa25be71fbdee1ad5c5df518c9c623fb9950
Varun <vjindz@gmail.com>
2018-04-29 07:27:48 -0400

    instantiate Analyzer

--- scraper.php
+++ scraper.php
@@ -9,7 +9,8 @@ require_once 'vadersentiment.php';
 		preg_match_all('/<span class="def-number">[0-9].<\/span>\s*<div class="def-content">(.*?)</s',
 			$content,
 			$matches);
-		$result = getSentiment($matches[1][0]);
+		$analyzer = SentimentIntensityAnalyzer();
+		$result = analyzer.getSentiment($matches[1][0]);
 		echo $result;
 		// $ex = "python parseText.py " . "\"stuff\"";
 		// $result = shell_exec($ex);
